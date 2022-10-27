937f32925c8509a26b9c458a634ce202cff2dad5
RubenOlander <rubenolander@gmail.com>
2022-10-27 16:43:11 +0200

    early draft

---
+++ arrays.php
@@ -0,0 +1,79 @@
+<?php
+
+declare(strict_types=1);
+$teamToyota = "Toyota Gazoo Racing World Rally Team";
+$teamHyundai = "Hyundai Shell Mobis World Rally Team";
+$teamMsport = "M-Sport Ford World Rally Team";
+
+$wrcDrivers = [
+    [
+        'name' => 'Kalle Rovanperä',
+        'season_points' => 255,
+        'co_driver' => 'Jonne Halttunen',
+        'event_wins_2022' => 6,
+        'team' => $teamToyota
+    ],
+    [
+        'name' => 'Ott Tänak',
+        'season_points' => 187,
+        'co_driver' => 'Martin Järveoja',
+        'event_wins_2022' => 3,
+        'team' => $teamHyundai
+    ],
+    [
+        'name' => 'Thierry Neuville',
+        'season_points' => 166,
+        'co_driver' => 'Martijn Wydaeghe',
+        'event_wins_2022' => 1,
+        'team' => $teamHyundai
+    ],
+    [
+        'name' => 'Elfyn Evans',
+        'season_points' => 124,
+        'co_driver' => 'Scott Martin',
+        'event_wins_2022' => 0,
+        'team' => $teamToyota
+    ],
+    [
+        'name' => 'Takamoto Katsuta',
+        'season_points' => 107,
+        'co_driver' => 'Aaron Johnston',
+        'event_wins_2022' => 0,
+        'team' => $teamToyota
+    ],
+    [
+        'name' => 'Sebastién Ogier',
+        'season_points' => 85,
+        'co_driver' => 'Benjamin Veillas',
+        'event_wins_2022' => 1,
+        'team' => $teamToyota,
+    ],
+    [
+        'name' => 'Craig Breen',
+        'season_points' => 79,
+        'co_driver' => 'Paul Nagle',
+        'event_wins_2022' => 0,
+        'team' => $teamMsport,
+    ],
+    [
+        'name' => 'Dani Sordo',
+        'season_points' => 59,
+        'co_driver' => 'Cándido Carrera',
+        'event_wins_2022' => 0,
+        'team' => $teamHyundai,
+    ],
+    [
+        'name' => 'Gus Greensmith',
+        'season_points' => 36,
+        'co_driver' => 'Jonas Andersson',
+        'event_wins_2022' => 0,
+        'team' => $teamMsport,
+    ],
+    [
+        'name' => 'Sebastién Loeb',
+        'season_points' => 35,
+        'co_driver' => 'Isabelle Galmiche',
+        'event_wins_2022' => 1,
+        'team' => $teamMsport,
+    ],
+];
--- index.php
+++ index.php
@@ -1,2 +1,35 @@
-<?= 'The Elephpent in the Room'; ?>
-<?= "Vad är livet, om inte rally"; ?>
+<?php
+
+declare(strict_types=1);
+require __DIR__ . '/header.php';
+require __DIR__ . '/arrays.php';
+require __DIR__ . '/functions.php';
+shuffle($wrcDrivers);
+?>
+
+<body>
+    <header class="rallyHero">
+        <a href="">Link to here to be placed in the corner</a>
+        <h1> Rubens WRC 2022 </h1>
+        <h2> Text som beskriver vad det här är för sida egentligen. Text som beskriver vad det här är för sida egentligen. Text som beskriver vad det här är för sida egentligen.</h2>
+        <main>
+    </header>
+    <article class="featuredDriver">
+        <?php echo "Name: ";
+        print_r($wrcDrivers[0]['name']);
+        echo nl2br("\nPoints in WRC 2022: ");
+        print_r($wrcDrivers[0]['season_points']);
+        echo nl2br("\nCo-Driver: ");
+        print_r($wrcDrivers[0]['co_driver']);
+        echo nl2br("\nWRC 2022 event wins: ");
+        print_r($wrcDrivers[0]['event_wins_2022']);
+        echo nl2br("\nTeam: ");
+        print_r($wrcDrivers[0]['team']);
+        ?>
+    </article>
+    <article> article2 som kanske listar vilka event som körts under året.</article>
+    <article> Breaking news</article>
+    <article> en till sån</article>
+    </main>
+
+</body>
